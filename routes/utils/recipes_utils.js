77d61fd5bf24b2d6ebf0bbdeced146612157843d
barakyac <64226145+barakyac@users.noreply.github.com>
2022-08-17 09:05:57 +0300

    update recipe utils

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const res = require("express/lib/response");
+const DButils = require("./DButils");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -10,14 +12,34 @@ const api_domain = "https://api.spoonacular.com/recipes";
 
 
 async function getRecipeInformation(recipe_id) {
-    return await axios.get(`${api_domain}/${recipe_id}/information`, {
+    let res = await axios.get(`${api_domain}/${recipe_id}/information`, {
         params: {
             includeNutrition: false,
             apiKey: process.env.spooncular_apiKey
         }
     });
+    return res;
 }
 
+async function getRandomReceipe(numOfRecipe){
+    
+    console.log(numOfRecipe);
+    const ans = await axios.get(`${api_domain}/random`, {
+        params: {
+            number: 3,
+            apiKey: process.env.spooncular_apiKey,
+        }
+    });
+
+    return ans;
+}
+
+
+// async function getRandomDetails(randoms){
+//     for (var i=0; i < randoms.length; i++) {
+//         console.log(randoms[i].id)
+//     }
+// }
 
 
 async function getRecipeDetails(recipe_id) {
@@ -37,9 +59,112 @@ async function getRecipeDetails(recipe_id) {
     }
 }
 
+const api_title ="https://api.spoonacular.com/recipes/complexSearch?query=";
+// async function getRecipeByTitle(title, numTitles){
+//     var receipes = await axios.get(`${api_title}/${title}&number=${numTitles}`, {
+//         params: {
+//             includeNutrition: false,
+//             apiKey: process.env.my_apiKey
+//         }
+//     });
+//     console.log(receipes);
+//     var res = new Array(numTitles);
+//     for (let i = 0; i < receipes.data.results.length; i++) {
+//         res[i] = receipes.data.results[i+1].id;
+//         res[i] = await getRecipeDetails(res[i]);
+//     }
+//     return res;
+
+// }
+
+async function getRecipeByTitle(title,numTitles){
+    let promises = [];
+    var last_watched_list = await DButils.execQuery(`select id from cakes_db.recips where  title=${title} `);
+    last_watched_list.map((id)=>{
+        promises.push(getRecipeInformation(id));
+    });
+    let info_res=await Promise.all(info_res);
+
+}
+
+async function IFUserEXIST(username) {
+    if(username === undefined){
+        return false
+    }
+    var last_watched = await DButils.execQuery(`select username from cakes_db.watchedbyuser `);
+
+    for (let i = 0; i < last_watched.length; i++){
+        if (last_watched[i]===username)
+            return true;
+    }
+    return false;
+}
+
+async function search(query,number,sort,cuisine, intolerance, Searchdiet) {
+    const response = await axios.get(`${api_domain}/complexSearch`, {
+        params: {
+            query: query,
+            diet: Searchdiet,	
+            number: number,
+            sort: sort,
+            cuisine: cuisine,
+            intolerances: intolerance,
+            apiKey: process.env.spooncular_apiKey
+        }
+        
+    });
+    return response
+}
+
+function extractPreviewRecipeDetails(recipes_info) {
+    return recipes_info.map((recipe_info) => {
+        //check the data type so it can work with diffrent types of data
+        let data = recipe_info;
+        if (recipe_info.data) {
+            data = recipe_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            image: image,
+            readyInMinutes: readyInMinutes,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree
+        }
+    })
+  }
+
+
+
+async function getRecipesPreview(recipes_ids_list) {
+    let promises = [];
+    recipes_ids_list.map((id) => {
+        promises.push(getRecipeInformation(id));
+    });
+    let info_res = await Promise.all(promises);
+    info_res.map((recp)=>{console.log(recp.data)});
+    // console.log(info_res);
+    return extractPreviewRecipeDetails(info_res);
+  }
 
 
 exports.getRecipeDetails = getRecipeDetails;
-
-
-
+exports.getRandomReceipe = getRandomReceipe;
+exports.getRecipeByTitle = getRecipeByTitle;
+exports.IFUserEXIST = IFUserEXIST;
+exports.search = search;
+exports.getRecipesPreview =getRecipesPreview;
+exports.getRecipeInformation = getRecipeInformation;
+// exports.getRandomDetails = getRandomDetails;
